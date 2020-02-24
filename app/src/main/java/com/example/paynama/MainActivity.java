a38b02e6ece6d42855b85c54ceb85de9f4867ae7
Bilal Qudoos <bilal7514@gmail.com>
2020-02-24 21:00:13 +0500

    Splash Designed

--- app/src/main/java/com/example/paynama/MainActivity.java
+++ app/src/main/java/com/example/paynama/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.paynama;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.Toast;
 
@@ -13,4 +15,5 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "hello", Toast.LENGTH_SHORT).show();
     }
+
 }
